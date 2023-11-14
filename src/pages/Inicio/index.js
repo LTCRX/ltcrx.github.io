f383e0274eb5cef8c2ce51bca39038ac0f516161
Evellyn Alexandra <Evellyn Alexandra>
2023-11-14 19:05:37 -0300

    creating routes

---
+++ src/pages/Inicio/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Inicio() {
+  return <h1>Inicio</h1>
+}
+
+export default Inicio;
---
+++ src/pages/Instalacoes/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+function Instalacoes() {
+  return <h1>Instalacoes</h1>
+}
+
+export default Instalacoes;
