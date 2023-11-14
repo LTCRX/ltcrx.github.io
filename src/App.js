a25b10d8efbefddec55fd054c42f6d20b80330ed
Evellyn Alexandra <Evellyn Alexandra>
2023-11-14 19:05:37 -0300

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import Home from "./components/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import {Stack} from "react-bootstrap";
-import Instalacoes from "./components/Instalacoes";
-import CorpoTecnico from "./components/CorpoTecnico";
+// import './App.css';
+// import Home from "./components/Home";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import {Stack} from "react-bootstrap";
+// import Instalacoes from "./components/Instalacoes";
+// import CorpoTecnico from "./components/CorpoTecnico";
+import RoutesApp from './routes';
 
 function App() {
     return (
         <div className="App">
-            <Stack>
+            {/* <Stack>
                 <Header/>
                 <Home/>
                 <Instalacoes/>
                 <CorpoTecnico/>
                 <Footer/>
-            </Stack>
+            </Stack> */}
+            <RoutesApp/>
         </div>
     );
 }
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Routers, Route, Routes} from 'react-router-dom'
+
+
+import Inicio from './pages/Inicio'
+import Instalacoes from './pages/Instalacoes'
+import CorpoTecnico from './pages/CorpoTecnico'
+import Agendamento from './pages/AgendamentodeServicos'
+import Header from './components/Header/Header'
+
+
+function RoutesApp(){
+    return(
+        <Routers>
+            <Header/>
+        <Routes>
+            <Route path="/" element={<Inicio/>}/>
+            <Route path="/Instalacoes" element={<Instalacoes/>}/>
+            <Route path="/CorpoTecnico" element={<CorpoTecnico/>}/>
+            <Route path="/AgendamentodeServicos" element={<Agendamento/>}/>
+        </Routes>
+        </Routers>
+    )
+}
+
+export default RoutesApp
