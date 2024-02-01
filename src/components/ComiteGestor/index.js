dcdf095c7202383d5cf1eeb518a3bb4c15fa8bee
Lucas Ravellys <57733801+ravellys@users.noreply.github.com>
2024-02-01 13:21:35 -0300

    fix comite gestor (#18)

--- src/components/ComiteGestor/index.js
+++ src/components/ComiteGestor/index.js
@@ -11,19 +11,19 @@ const ComiteGestor = () => {
             <Row>
                 <Col md={12}>
                     <p>
-                        Compõe o corpo técnico do LTC-RX técnicos, professores e pesquisadores de produtividade em
-                        pesquisa do CNPq que possuem reconhecimento e atuação nacional e internacional em pesquisas
+                        Compõe o corpo técnico do <b>LTC-RX</b> técnicos, professores e pesquisadores de produtividade em
+                        pesquisa do <b>CNPq</b> que possuem reconhecimento e atuação nacional e internacional em pesquisas
                         multidisciplinares.
                         <br/>
                         Vários desses professores são orientadores acadêmicos de alunos de mestrado e doutorado no
                         Programa de Pós-Graduação em Energia e Tecnologias Nucleares do Departamento de Energia
-                        Nuclear (DEN) da Universidade Federal de Pernambuco (UFPE).
+                        Nuclear (<b>DEN</b>) da Universidade Federal de Pernambuco (<b>UFPE</b>).
                     </p>
                 </Col>
             </Row>
             <Row>
                 <Col md={12} className="p-3">
-                    <h3>Comitê Gestor</h3>.
+                    <h3>Comitê Gestor</h3>
                     <Container fluid>
                         <Stack direction="horizontal" gap={5} className="justify-content-center align-items-center">
                             <ComiteProfileCard
