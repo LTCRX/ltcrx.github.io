82df39a690554d740fdc23c2978dab67b5d822a3
ravellys <ravellyspyrrho@gmail.com>
2024-02-01 19:30:28 -0300

    feature: localizacao page

---
+++ src/components/Title/TItleLocalizacao/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <p
+        className="fs-1 text-center"
+        style={{ marginTop: 50, marginBottom: 50 }}
+      >
+        Localização
+      </p>
+    );
+  }
+}
