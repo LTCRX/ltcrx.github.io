f949e93fb8271b7decaae715e5fc9c15d53ecb9f
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-01 16:56:45 -0300

    localization screen

--- src/components/Localizacao/index.js
+++ src/components/Localizacao/index.js
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 export default class index extends Component {
   render() {
     return (
-      <iframe
-        className="location-card"
-        title="Mapa incorporado"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15801.941298474445!2d-34.960614952063814!3d-8.051874821606747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1966d409d415%3A0x497df2e2ee00ea52!2sUniversidade%20Federal%20de%20Pernambuco!5e0!3m2!1spt-BR!2sbr!4v1700641687501!5m2!1spt-BR!2sbr"
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <>
+        <iframe
+          className="location-card vh-100"
+          title="Mapa incorporado"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d493.80367428136105!2d-34.955177!3d-8.057607!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab1be3683ae62b%3A0xd5fa3ad91e24df9d!2sDepartamento%20de%20Energia%20Nuclear%20(DEN)!5e0!3m2!1spt-BR!2sbr!4v1706816684487!5m2!1spt-BR!2sbr"
+          style={{ border: 0, width: "80%", height: "60%" }}
+          allowFullScreen="true"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </>
     );
   }
 }
--- src/pages/Localizacao.js
+++ src/pages/Localizacao.js
@@ -1,9 +1,24 @@
 import React from "react";
+import "../Assets/css/Global.css";
+import TitleLocalizacao from "../components/Title/TItleLocalizacao";
+import GpsLocalizacao from "../components/Localizacao";
 
 function Localizacao() {
   return (
     <>
-      <h1>Localizacao</h1>
+      <div className="container-fluid" id="background">
+        <div className="row justify-content-center align-items-center 1000vh">
+          <div className="col-12 col-md-8 text-center">
+            <TitleLocalizacao />
+            <GpsLocalizacao />
+            <br />
+            <p className="fs-3 text-center" style={{ marginBottom: 100 }}>
+              Av. Prof. Luiz Freire, 1000 - Cidade Universitária, Recife - PE,
+              50740-437
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
