50075cc8b5fbda941387aea256e35f021cb736f9
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-01 22:19:44 -0300

    separate component

---
+++ src/components/Text/TextCorpoTecnico/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class index extends Component {
+  render() {
+    return (
+      <p className="fs-6">
+        Compõe o corpo técnico do LTC-RX técnicos, professores e pesquisadores
+        de produtividade em pesquisa do CNPq que possuem reconhecimento e
+        atuação nacional e internacional em pesquisas multidisciplinares. Vários
+        desses professores são orientadores acadêmicos de alunos de mestrado e
+        doutorado no Programa de Pós-Graduação em Energia e Tecnologias
+        Nucleares do Departamento de Energia Nuclear (DEN) da Universidade
+        Federal de Pernambuco (UFPE).
+      </p>
+    );
+  }
+}
