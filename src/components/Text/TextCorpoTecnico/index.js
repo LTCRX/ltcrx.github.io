7e77414beb14851a1b3148ff13d17f05ec4bf799
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-01 23:59:24 -0300

    feature instalacoes (#28)

--- src/components/Text/TextCorpoTecnico/index.js
+++ src/components/Text/TextCorpoTecnico/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default class index extends Component {
   render() {
     return (
-      <p className="fs-6">
+      <p className="fs-6" style={{ marginTop: 50, marginBottom: 50 }}>
         Compõe o corpo técnico do LTC-RX técnicos, professores e pesquisadores
         de produtividade em pesquisa do CNPq que possuem reconhecimento e
         atuação nacional e internacional em pesquisas multidisciplinares. Vários
