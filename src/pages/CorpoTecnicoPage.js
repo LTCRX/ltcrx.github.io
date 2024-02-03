3959dab680adc4b932db05757441947a8db14adc
Lucas Ravellys <57733801+ravellys@users.noreply.github.com>
2024-02-03 11:28:46 -0300

    minor fixis (#32)

--- src/pages/CorpoTecnicoPage.js
+++ src/pages/CorpoTecnicoPage.js
@@ -7,6 +7,8 @@ import CoordenadorProfileCard from "../components/Card/CoordenadorCard";
 import ViceCoordenadorProfileCard from "../components/Card/ViceCoordenadorCard";
 
 function CorpoTecnico() {
+    const direction = window.innerWidth <= 768
+
     return (
         <Container>
             <Row>
@@ -18,12 +20,13 @@ function CorpoTecnico() {
                 <Col md={12} className="p-1">
                     <TitleComite/>
                     <Container fluid>
-                        <Stack direction="horizontal"
+                        <Stack direction={window.innerWidth <= 768 ? "vertical" : "horizontal"}
                                gap={5}
                                className="justify-content-center align-items-center">
                             <CoordenadorProfileCard/>
                             <ViceCoordenadorProfileCard/>
                         </Stack>
+
                     </Container>
                 </Col>
             </Row>
