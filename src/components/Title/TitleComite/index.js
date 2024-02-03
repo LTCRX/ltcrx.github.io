3a175b25e5b52b45b6f6f659d6adcdebb5a10ab1
geanderson062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-03 10:36:40 -0300

    feature/loading screen (#30)
    * loading screen
    
    * fixing CorpoTecnico.ja
    
    ---------
    
    Co-authored-by: ravellys <ravellyspyrrho@gmail.com>

--- src/components/Title/TitleComite/index.js
+++ src/components/Title/TitleComite/index.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class index extends Component {
   render() {
-    return <p className="fs-4">Comitê Gestor</p>;
+    return <h2 className="fs-4">Comitê Gestor</h2>;
   }
 }
--- src/pages/CorpoTecnico.js
+++ src/pages/CorpoTecnico.js
@@ -1,23 +1,34 @@
 import React from "react";
 import "../Assets/css/CorpoTecnico.css";
-import CardCoordenador from "../components/Card/AntominoCard";
-import CardViceCoordenador from "../components/Card/AntonioCard";
 import TextCorpoTecnico from "../components/Text/TextCorpoTecnico";
 import TitleComite from "../components/Title/TitleComite";
+import {Col, Container, Row, Stack} from "react-bootstrap";
+import CoordenadorProfileCard from "../components/Card/CoordenadorCard";
+import ViceCoordenadorProfileCard from "../components/Card/ViceCoordenadorCard";
 
 function CorpoTecnico() {
-  return (
-    <div className="corpo-tecnico-container">
-      <div className="corpo-tecnico-content">
-        <TextCorpoTecnico />
-        <TitleComite />
-      </div>
-      <div className="corpo-tecnico-cards">
-        <CardCoordenador />
-        <CardViceCoordenador />
-      </div>
-    </div>
-  );
+    return (
+        <Container>
+            <Row>
+                <Col md={12}>
+                    <TextCorpoTecnico/>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={12} className="p-3">
+                    <TitleComite/>
+                    <Container fluid>
+                        <Stack direction="horizontal"
+                               gap={5}
+                               className="justify-content-center align-items-center">
+                            <CoordenadorProfileCard/>
+                            <ViceCoordenadorProfileCard/>
+                        </Stack>
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default CorpoTecnico;
